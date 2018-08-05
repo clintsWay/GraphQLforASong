5efa11483df732254163516c4cba267afe29593f
Clint Ast <clintast@gmail.com>
2018-08-05 14:50:46 -0500

    saving some mutations

--- client/components/SongCreate.js
+++ client/components/SongCreate.js
@@ -1,7 +1,8 @@
 import React, { Component } from 'react';
 import gql from 'graphql-tag';
 import { graphql } from 'react-apollo';
-import { Link } from 'react-router';
+import { Link, hashHistory } from 'react-router';
+import query from '../queries/fetchSongs';
 
 
 class SongCreate extends Component {
@@ -15,10 +16,9 @@ class SongCreate extends Component {
     event.preventDefault();
 
     this.props.mutate({
-      variables: {
-        title: this.state.title
-      }
-    });
+      variables: { title: this.state.title},
+      refetchQueries: [{query}]
+    }).then(() => hashHistory.push('/'))
   }
 
   render() {
--- client/components/SongList.js
+++ client/components/SongList.js
@@ -2,15 +2,29 @@ import React, {Component} from 'react';
 import gql from 'graphql-tag';
 import { graphql } from 'react-apollo';
 import { Link } from 'react-router';
+import query from '../queries/fetchSongs';
 
 class SongList extends Component {
+  onSongDelete(id) {
+    this.props.mutate({ variables: { id }})
+      .then(() => this.props.data.refetch());
+  }
+
   renderSongs() {
    
-    return this.props.data.songs.map(song => {
+    return this.props.data.songs.map(({id, title}) => {
       return (
         // key = { song.id }
-        <li key={song.id} className="collection-item">
-          {song.title}
+        <li key={id} className="collection-item">
+          <Link to={`/songs/${id}`}>
+            {title}
+          </Link>
+         
+          <i
+            className="material-icons"
+            onClick={() => this.onSongDelete(id)}
+          >
+            delete</i>
         </li>
       );
     })
@@ -35,13 +49,18 @@ class SongList extends Component {
   }
 }
 
-const query = gql`
-  {
-    songs {
+const mutation = gql`
+  mutation DeleteSong($id: ID) {
+    deleteSong(id: $id) {
       id
       title
     }
-  }
+}
+
 `;
 
-export default graphql(query)(SongList);
+
+
+export default graphql(mutation)(
+  graphql(query)(SongList)
+);
